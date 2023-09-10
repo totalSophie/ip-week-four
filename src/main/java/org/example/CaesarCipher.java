2e9ed994fed2027b91f5ac0b0afc26c3291b00e8
Sophia Chisiya <totalsophie@gmail.com>
2023-09-10 08:29:42 +0300

    Initial commit

---
+++ src/main/java/org/example/CaesarCipher.java
@@ -0,0 +1,44 @@
+package org.example;
+
+public class CaesarCipher {
+
+    private int key;
+
+    public CaesarCipher(int key) {
+        this.key = key;
+    }
+
+    public int getKey() {
+        return key;
+    }
+
+    public void setKey(int key) {
+        this.key = key;
+    }
+
+    public String encrypt(String text) {
+        return processText(text, key);
+    }
+
+    public String decrypt(String text) {
+        return processText(text, 26 - key);
+    }
+    public String processText(String text, int key) {
+        StringBuilder encryptedResult = new StringBuilder();
+
+        for (char character : text.toCharArray()){
+            // Shift only if it is a letter
+            if (Character.isLetter(character)){
+                char baseLetter = Character.isLowerCase(character)? 'a' : 'A';
+                char shiftedLetter = (char) (((character - baseLetter + key) % 26) + baseLetter);
+                encryptedResult.append(shiftedLetter);
+            }
+            else {
+                encryptedResult.append(character);
+            }
+        }
+
+        return encryptedResult.toString();
+    }
+
+}
---
+++ src/test/java/CaesarCipherTest.java
@@ -0,0 +1,30 @@
+import org.example.CaesarCipher;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class CaesarCipherTest {
+    @Test
+    public void testCipher() {
+        CaesarCipher cipher = new CaesarCipher(2);
+        assertEquals("HI", cipher.decrypt("JK"));
+        assertEquals("JGNNQ", cipher.encrypt("HELLO"));
+        assertEquals("Hello, World!", cipher.decrypt("Jgnnq, Yqtnf!"));
+    }
+
+    @Test
+    public void testKeyChange() {
+        CaesarCipher cipher = new CaesarCipher(3);
+
+        assertEquals("KL", cipher.encrypt("HI"));
+        assertEquals("HI", cipher.decrypt("KL"));
+        assertEquals("QEB NRFZH YOLTK CLU GRJMP LSBO QEB IXWV ALD", cipher.decrypt("THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG"));
+        assertEquals("THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG", cipher.encrypt("QEB NRFZH YOLTK CLU GRJMP LSBO QEB IXWV ALD"));
+
+        cipher.setKey(5);
+
+        assertEquals("MN", cipher.encrypt("HI"));
+        assertEquals("HI", cipher.decrypt("MN"));
+
+    }
+}
